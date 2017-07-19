5dd8f38c46ee42b4f587a85c099d9a8eff7573ec
Robert Baumgartner <rbaumgar@redhat.com>
2017-07-19 18:13:04 +0200

    typo

--- sampleapp/container-scripts/app-deploy.py
+++ sampleapp/container-scripts/app-deploy.py
@@ -12,6 +12,7 @@ import os
 
 # Deployment Information 
 domain_name  = os.environ.get("DOMAIN_NAME", "base_domain")
+domain_home  = os.environ.get('DOMAIN_HOME', '/u01/oracle/user_projects/domains/base_domain')
 admin_name   = os.environ.get('ADMIN_NAME', 'AdminServer')
 app_name     = os.environ.get('APP_NAME', 'sample')
 app_file     = os.environ.get('APP_FILE', 'sample.war')
@@ -20,6 +21,7 @@ cluster_name = os.environ.get("CLUSTER_NAME", "DockerCluster")
 
 print('Deploy an application');
 print('domain_name     : [%s]' % domain_name);
+print('domain_home     : [%s]' % domain_home);
 print('admin_name      : [%s]' % admin_name);
 print('app_name        : [%s]' % app_name);
 print('app_file        : [%s]' % app_file);
